89851e9963bf13870f7dfbb83d03af84c69835de
LAI-Osamu-Sakane <sakane.osamu@link-academy.co.jp>
2024-04-22 10:28:44 +0900

    24課題終了後

--- src/main/java/raisetech/StudentsManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentsManagement/service/StudentService.java
@@ -1,6 +1,9 @@
 package raisetech.StudentsManagement.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.StudentsManagement.data.Student;
@@ -18,11 +21,25 @@ public class StudentService {
   }
 
   public List<Student> searchStudentList() {
-    return repository.search();
+    //　24課題ここから
+    List<Student>resultList = repository.search().stream()
+        .filter(result -> result.getAge() >= 30 && result.getAge() <= 39)
+        .collect(Collectors.toList());
+
+    return resultList;
+    // 24課題ここまで
+//    return repository.search(); // 24課題作成時にコメントアウトすること
   }
 
   public List<StudentsCourses> searchStudentsCourseList() {
-    return repository.searchStudentsCourses();
+    //　24課題ここから
+    List<StudentsCourses> resultList = repository.searchStudentsCourses().stream()
+        .filter(result -> result.getCourseName().matches(".*Java.*"))
+        .collect(Collectors.toList());
+
+    return resultList;
+    // 24課題ここまで
+//    return repository.searchStudentsCourses();  // 24課題作成時にコメントアウトすること
   }
 
 }
