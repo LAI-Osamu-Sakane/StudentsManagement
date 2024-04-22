13945bc9c2dbcbcae17648083cb18484dc6dd0bb
LAI-Osamu-Sakane <sakane.osamu@link-academy.co.jp>
2024-04-22 20:01:23 +0900

    27映像終了後

--- src/main/java/raisetech/StudentsManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentsManagement/controller/StudentController.java
@@ -1,31 +1,63 @@
 package raisetech.StudentsManagement.controller;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentsManagement.controller.converter.StudentConverter;
 import raisetech.StudentsManagement.data.Student;
 import raisetech.StudentsManagement.data.StudentsCourses;
+import raisetech.StudentsManagement.domain.StudentDetail;
 import raisetech.StudentsManagement.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public String getStudentList(Model model) {
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
+
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentsCourseList")
   public List<StudentsCourses> getStudentsCourseList() {
     return service.searchStudentsCourseList();
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if(result.hasErrors()) {
+      return "registerStudent";
+    }
+    System.out.println(studentDetail.getStudent().getName() + "さんが受講生として登録されました");
+    //新規受講生登録処理を実装する
+    //コース情報も登録できるように実装する（単体でよい)
+    return "redirect:/studentList";
   }
 }
--- src/main/java/raisetech/StudentsManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentsManagement/service/StudentService.java
@@ -1,9 +1,7 @@
 package raisetech.StudentsManagement.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raisetech.StudentsManagement.data.Student;
@@ -21,25 +19,11 @@ public class StudentService {
   }
 
   public List<Student> searchStudentList() {
-    //　24課題ここから
-    List<Student>resultList = repository.search().stream()
-        .filter(result -> result.getAge() >= 30 && result.getAge() <= 39)
-        .collect(Collectors.toList());
-
-    return resultList;
-    // 24課題ここまで
-//    return repository.search(); // 24課題作成時にコメントアウトすること
+    return repository.search();
   }
 
   public List<StudentsCourses> searchStudentsCourseList() {
-    //　24課題ここから
-    List<StudentsCourses> resultList = repository.searchStudentsCourses().stream()
-        .filter(result -> result.getCourseName().matches(".*Java.*"))
-        .collect(Collectors.toList());
-
-    return resultList;
-    // 24課題ここまで
-//    return repository.searchStudentsCourses();  // 24課題作成時にコメントアウトすること
+    return repository.searchStudentsCourses();
   }
 
 }
